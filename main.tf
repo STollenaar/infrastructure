28104fa47fc1794c806c2c0feacc2c556834b021
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2022-07-21 21:12:53 -0230

    optimizing instance from findings

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "aws_instance" "bot_instance" {
   ami                                  = "ami-0e4208b856d700843"
   associate_public_ip_address          = true
   disable_api_termination              = false
-  ebs_optimized                        = false
+  ebs_optimized                        = true
   get_password_data                    = false
   hibernation                          = false
   iam_instance_profile                 = aws_iam_role.spices_role.id
   instance_initiated_shutdown_behavior = "stop"
-  instance_type                        = "t4g.medium"
+  instance_type                        = "t4g.micro"
   key_name                             = "stollenaar"
   monitoring                           = false
   source_dest_check                    = true
@@ -34,11 +34,13 @@ resource "aws_instance" "bot_instance" {
       "Name" = "Spices"
     }
     volume_size = 40
-    volume_type = "gp2"
+    volume_type = "gp3"
   }
 
   user_data = templatefile("${path.module}/conf/discordBotInits.bash.tpl", {
-    dockerCompose = filebase64("${path.module}/conf/discordbots-compose.yml")
+    dockerCompose         = filebase64("${path.module}/conf/discordbots-compose.yml"),
+    cloudwatchAgentSchema = filebase64("${path.module}/conf/cloudwatch-agent-config.json")
+    accountID             = data.aws_caller_identity.current.account_id
   })
 }
 
@@ -46,3 +48,4 @@ resource "aws_eip" "spices_ip" {
   instance = aws_instance.bot_instance.id
   vpc      = true
 }
+
