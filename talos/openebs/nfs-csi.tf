d1d1c7320063b0231e03d29c291bdfcf3b494bba
Stephen Tollenaar <stephen@tollenaar.com>
2023-11-09 19:32:08 -0330

    fixing nfs csi charts

--- talos/openebs/nfs-csi.tf
+++ talos/openebs/nfs-csi.tf
@@ -1,9 +1,25 @@
-resource "helm_release" "nfs_csi" {
+resource "helm_release" "nfs_csi_movies" {
   repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/"
 
-  name      = "nfs-subdir-external-provisioner"
+  name      = "nfs-subdir-external-provisioner-movies"
   chart     = "nfs-subdir-external-provisioner"
   namespace = kubernetes_namespace.openebs.metadata.0.name
 
-  values = [file("${path.module}/conf/nfs-csi-values.yaml")]
+  values = [templatefile("${path.module}/conf/nfs-csi-values.yaml", {
+    nfs_path           = "/mnt/storage/kubernetes"
+    storage_class_name = "nfs-client-movies"
+  })]
+}
+
+resource "helm_release" "nfs_csi_other" {
+  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/"
+
+  name      = "nfs-subdir-external-provisioner-other"
+  chart     = "nfs-subdir-external-provisioner"
+  namespace = kubernetes_namespace.openebs.metadata.0.name
+
+  values = [templatefile("${path.module}/conf/nfs-csi-values.yaml", {
+    nfs_path           = "/mnt/storage2/kubernetes"
+    storage_class_name = "nfs-client-other"
+  })]
 }
