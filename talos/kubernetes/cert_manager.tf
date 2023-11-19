482a8a7e061679bdb1dc5ceecf365f410ad5e538
Stephen Tollenaar <stephen@tollenaar.com>
2023-11-19 19:32:44 -0330

    adding cert-manager

---
+++ talos/kubernetes/cert_manager.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+    labels = {
+      "cmstate.spices.dev" = "opt-out"
+    }
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name        = "cert-manager"
+  chart       = "cert-manager"
+  version     = "v1.13.2"
+  repository  = "https://charts.jetstack.io"
+  namespace   = kubernetes_namespace.cert_manager.metadata.0.name
+  wait        = false
+  max_history = 50
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
