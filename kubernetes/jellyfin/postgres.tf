734837e667d0878a2db95fd79548bee0c8646533
Stephen Tollenaar <stephen@tollenaar.com>
2025-07-31 14:22:05 -0230

    update to cpg postgres

--- kubernetes/jellyfin/postgres.tf
+++ kubernetes/jellyfin/postgres.tf
@@ -85,3 +85,81 @@ resource "kubernetes_service" "postgres" {
     }
   }
 }
+
+resource "kubernetes_manifest" "cnpg_cluster" {
+  manifest = {
+    apiVersion = "postgresql.cnpg.io/v1"
+    kind       = "Cluster"
+    metadata = {
+      name      = "postgres"
+      namespace = kubernetes_namespace.jellyfin.id
+    }
+    spec = {
+      instances             = 1
+      imageName             = "ghcr.io/cloudnative-pg/postgresql:16"
+      primaryUpdateStrategy = "unsupervised"
+      postgresUID           = 1000
+      postgresGID           = 1000
+
+      superuserSecret = {
+        name = kubernetes_secret_v1.postgres_superuser.metadata.0.name
+      }
+      monitoring = {
+        enablePodMonitor = true
+      }
+      bootstrap = {
+        initdb = {
+          import = {
+            type = "monolith"
+            databases = [
+              "bazarr-main",
+              "radarr-main",
+              "radarr-log",
+              "sonarr-main",
+              "sonarr-log",
+              "prowlarr-main",
+              "prowlarr-log",
+              "whisparr-main",
+              "whisparr-log",
+            ]
+            source = {
+              externalCluster = "jellyfin"
+            }
+          }
+        }
+      }
+      storage = {
+        storageClass = "nfs-csi-main"
+        size         = "20Gi"
+      }
+      externalClusters = [
+        {
+          name = "jellyfin"
+          connectionParameters = {
+            host     = "${kubernetes_service.postgres.metadata.0.name}.${kubernetes_namespace.jellyfin.id}"
+            user     = "admin"
+            dbname   = "postgres"
+          }
+          password = {
+            name = kubernetes_secret_v1.postgres_superuser.metadata.0.name
+            key  = "password"
+          }
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_secret_v1" "postgres_superuser" {
+  metadata {
+    name      = "postgres-superuser-secret"
+    namespace = kubernetes_namespace.jellyfin.id
+  }
+
+  type = "Opaque"
+
+  data = {
+    username = "admin"
+    password = "password"
+  }
+}
