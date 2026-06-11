9383af02ad62331f181d2f8f19d6d41e031bc973
Stephen Tollenaar <1guitargun1@gmail.com>
2026-06-11 18:17:04 -0230

    Revert "chore(deps): Update terraform providers to v8"

--- kubernetes/kubehound.tf
+++ kubernetes/kubehound.tf
@@ -122,7 +122,7 @@ resource "kubernetes_stateful_set_v1" "kubehound_mongo" {
       spec {
         container {
           name  = "mongodb"
-          image = "mongo:8.3.3"
+          image = "mongo:6.0.28"
           port {
             container_port = 27017
           }
